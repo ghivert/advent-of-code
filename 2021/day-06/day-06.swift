0fd5bece4d940cf6ff7e471c881da391e500c90d
Guillaume Hivert <hivert.is.coming@gmail.com>
2021-12-07 14:55:17 +0100

    :art: Improve Day 06
    Signed-off-by: Guillaume Hivert <hivert.is.coming@gmail.com>

--- 2021/day-06/day-06.swift
+++ 2021/day-06/day-06.swift
@@ -10,30 +10,27 @@ func readValues() throws -> [Int] {
   return trimmed.components(separatedBy: ",").map(toInt)
 }
 
-func first() {
+func computation(_ generations: Int) -> Int {
   let values = try! readValues()
   var fishes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
   for i in values { fishes[i] = fishes[i] + 1 }
-  for _ in 0 ..< 80 {
+  for _ in 0 ..< generations {
     let zeros = fishes[0]
-    (0..<fishes.count).forEach { if $0 != 8 { fishes[$0] = fishes[$0 + 1] } }
+    (0 ..< fishes.count).forEach { if $0 != 8 { fishes[$0] = fishes[$0 + 1] } }
     fishes[8] = zeros
     fishes[6] = fishes[6] + zeros
   }
-  print(fishes.reduce(0, +))
+  return fishes.reduce(0, +)
+}
+
+func first() {
+  let value = computation(80)
+  print(value)
 }
 
 func second() {
-  let values = try! readValues()
-  var fishes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-  for i in values { fishes[i] = fishes[i] + 1 }
-  for _ in 0 ..< 256 {
-    let zeros = fishes[0]
-    (0..<fishes.count).forEach { if $0 != 8 { fishes[$0] = fishes[$0 + 1] } }
-    fishes[8] = zeros
-    fishes[6] = fishes[6] + zeros
-  }
-  print(fishes.reduce(0, +))
+  let value = computation(256)
+  print(value)
 }
 
 second()
